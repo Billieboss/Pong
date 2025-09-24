18be297e745756e20a66bf36b5b60fc7b70c953d
Camilleeeeeeeeee <camille.golliot06@gmail.com>
2025-09-24 13:07:21 +0200

    deplacements boutons et tactile fontionnels + refactor deplacements clavier

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById("zoneJeu");
 const ctx = canvas.getContext("2d");
 const initButton = document.getElementById("newGame-btn");
 const scoreDisplay = document.getElementById("score");
-const gauche = document.getElementById("gauche");
-const droite = document.getElementById("droite");
+const left = document.getElementById("gauche");
+const right = document.getElementById("droite");
 
 const cursorWidth = canvas.width*0.15;
 const ballRadius = canvas.width*0.015;
@@ -14,6 +14,9 @@ let baseSpeed = 2;
 let speedX;
 let speedY;
 let rafId;
+
+let moveLeft = false;
+let moveRight = false;
 
 let score = 0;
 let intervalScore;
@@ -80,8 +83,18 @@ function update(){
     
 }
 
+function updateCursor(){
+    if(moveLeft && cursorX > 0){
+        cursorX -= 5
+    }
+    if(moveRight&& cursorX + cursorWidth < canvas.width){
+        cursorX += 5
+    }
+}
+
 function loop(){
     update();
+    updateCursor();
     redrawAll();
     rafId = requestAnimationFrame(loop);
 
@@ -95,18 +108,32 @@ function updateScore(){
 document.addEventListener('keydown', (e) =>{
     switch(e.key){
         case "ArrowLeft":
-            if(cursorX > 0) cursorX -= 15;
+            moveLeft = true;
             break;
 
         case "ArrowRight":
-            if(cursorX + cursorWidth < canvas.width) cursorX += 15;
-            break;
+            moveRight = true
+        break;
         
         default:
             break;
     }
-    redrawAll();
-})
+});
+
+document.addEventListener('keyup', (e) =>{
+    switch(e.key){
+        case "ArrowLeft":
+            moveLeft = false;
+            break;
+
+        case "ArrowRight":
+            moveRight = false;
+        break;
+        
+        default:
+            break;
+    }
+});
 
 initButton.addEventListener('click', () =>{
     rafId = 0;
@@ -118,7 +145,19 @@ initButton.addEventListener('click', () =>{
     loop();   
 });
 
+left.addEventListener('mousedown', () => moveLeft = true);
+left.addEventListener('mouseup', () => moveLeft = false);
+right.addEventListener('mousedown', () => moveRight = true);
+right.addEventListener('mouseup', () => moveRight = false);
+
+left.addEventListener('touchstart', () => moveLeft = true);
+left.addEventListener('touchend', () => moveLeft = false);
+right.addEventListener('touchstart', () => moveRight = true);
+right.addEventListener('touchend', () => moveRight = false);
+
+
 function gameOver(){
+    cancelAnimationFrame(rafId);
     clearInterval(intervalScore);
     ctx.font = 'bold 20px Verdana, Arial, serif';
     ctx.fillStyle = 'pink';
